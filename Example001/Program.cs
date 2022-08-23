48a74c8accb1052321d75f99d1117297c47afb75
tati <ost11tan@gmail.com>
2022-08-23 04:05:33 +0300

    Задача 1 квадрат числа

---
+++ Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Результат = {0}",sqr);
+Console.WriteLine($"Результат = {sqr}"); //$ нужен,чтоб захватывать переменную,а не строку
+
+
+ // int result=Convert.ToInt32(Math.Pow(number, 2)); // конdертация из дабл в инт
+Console.WriteLine($"Результат={Math.Pow(number,2)}");// вывод с помощью библиотеки math
+

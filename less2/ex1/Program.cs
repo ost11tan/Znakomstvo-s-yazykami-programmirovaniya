6627db73bdcda2dbfba42dc4999ab75c9413543e
tati <ost11tan@gmail.com>
2022-08-26 03:57:57 +0300

    пример из семинара

---
+++ less2/ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10, 100); 
+// ///////////////[a,b) создаю новое случайное чило от  [10,100)
+int a1 = num % 10;
+int a2 = num / 10;
+
+
+int max = a1;
+if (max <a2)
+{
+    max = a2;
+
+}
+Console.WriteLine($"{num}, max:{max}");

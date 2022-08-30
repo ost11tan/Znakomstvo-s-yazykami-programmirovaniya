9eb7d800823f0e98d3390920793712faec4aff78
tati <ost11tan@gmail.com>
2022-08-31 01:11:33 +0300

    оформление функции

---
+++ less4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число А: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetNums(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+Console.WriteLine($"{GetNums(num)}");

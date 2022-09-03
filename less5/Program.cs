28b012f6ec710be7007c42d805f458a86b813815
tati <ost11tan@gmail.com>
2022-09-03 03:47:56 +0300

    пример с семинара

---
+++ less5/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+res[i] = new Random().Next(minValue, maxValue + 1);
+}
+return res;
+}
+
+int[] PositiveAndNegativeSum(int[] array)
+{
+int[] res = new int[2];
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > 0)
+{
+res[0] += array[i];
+}
+else
+{
+res[1] += array[i];
+}
+
+}
+
+return res;
+}
+
+int[] myArray = GetArray(10,-100, 100);
+int[] result = PositiveAndNegativeSum(myArray);
+Console.WriteLine(String.Join(" ", myArray));
+Console.WriteLine(String.Join(" ", result));
